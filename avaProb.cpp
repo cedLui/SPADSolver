4a0d3be3d3ee31c7f03586bbb95c9b55e9be3df1
Ishaan <ishaanranjan@gmail.com>
2026-07-29 10:47:44 -0700

    Partially replicated Oldham paper results, non-trivial solutions appear

--- avaProb.cpp
+++ avaProb.cpp
@@ -3,17 +3,72 @@
 #include <vector> 
 #include "avaProb.h"
 
-//These are the impact ionization coefficients for the material. Currently they are constant, but they can be changed to be functions of position
-double alphaX(double Pos){
-    return 0.3;
+//These are the impact ionization coefficients for the material. They are taken from the Maeda paper
+double alphaX(double DeltaV, double Pos, double Width){ //In cm^-1. EField is in V/cm, T is in Kelvin
+    /*double a_n =  2.69e7; // cm^-1
+    double b_n = 2.00e-3; // K^-1
+    double c_n = 2.27e7; // V * cm^-1
+    double d_n = 5.00e-4; // K^-1
+
+    return a_n*(1 + b_n*(T - 298)) * std::exp(-c_n*(1+d_n*(T - 298))/EField);*/
+    double V_break = 27.08;
+    double E = 2*(V_break + DeltaV)*(1 - Pos/Width)/Width; //Electric field profile for n+p junction
+    return 3.8 * 1000000 * std::exp(-1.75*1000000/E);
 }
 
-double betaX(double Pos){
-    return 0.2;
+double betaX(double DeltaV, double Pos, double Width){ //In cm^-1. EField is in V/cm, T is in Kelvin
+    /*double a_p =  4.32e6; // cm^-1
+    double b_p = 2.00e-3; // K^-1
+    double c_p = 1.31e7; // V * cm^-1
+    double d_p = 9.00e-4; // K^-1
+
+    return a_p*(1 + b_p*(T - 298)) * std::exp(-c_p*(1+d_p*(T - 298))/EField);*/
+    double V_break = 27.08;
+    double E = 2*(V_break + DeltaV)*(1 - Pos/Width)/Width; //Electric field profile for n+p junction
+    return 2.25 * 10000000 * std::exp(-3.26 * 1000000/E);
+}
+
+std::vector<double> guess100(double Width, int Steps, double DeltaV){
+    double StepSize = Width/Steps;
+
+    std::vector<double> Guesses;
+    std::vector<double> F; //Holds P_h.at(Steps)
+
+    std::vector<double> P_e;
+    std::vector<double> P_h;
+
+    for (int i = 0; i < 100; i++){ //Initializes a guess for every 0.01
+        Guesses.push_back(i/100.0);
+    }
+
+    for (int j = 0; j < Steps + 1; j++){ //Initialize P_e and P_h
+        P_e.push_back(0);
+        P_h.push_back(0);
+    }
+
+    for (double Guess : Guesses){ //For each guess, run the solver
+        P_e.at(0) = 0.0; //Reinit P_e(0) = 0
+        P_h.at(0) = Guess; //Init guess
+
+        for (int k = 0 ; k<Steps; k++){
+            P_e.at(k+1) = P_e.at(k) + (1- P_e.at(k)) * alphaX(DeltaV, StepSize*k, Width) * (P_e.at(k)+P_h.at(k)-P_e.at(k)*P_h.at(k)) * StepSize;
+            P_h.at(k+1) = P_h.at(k) - (1- P_h.at(k)) * betaX(DeltaV, StepSize*k, Width) * (P_h.at(k)+P_e.at(k)-P_e.at(k)*P_h.at(k)) * StepSize;
+        }
+        F.push_back(P_h.at(Steps)); //Creates a vector of all P_h(W), which should be close to zero
+    }
+
+    for (int i = 2; i<Steps + 1; i++){
+        if (std::abs(F[i-1] + F[i]) != std::abs(F[i-1]) + std::abs(F[i])){
+            std::cout << i-1 << std::endl;
+            std::cout << i << std::endl;
+        }
+    }
+
+    return F;
 }
 
 //This function will return 2 vectors: 1st a descretized version of P_e, and the 2nd a descretized version of P_h
-std::vector<std::vector<double>> avaProb(double Width, int Steps, double P_h_0, double Accuracy){
+std::vector<std::vector<double>> avaProb(double Width, int Steps, double P_h_0, double Accuracy, double DeltaV){
     int LoopCount = 0; //Counts how many iterations
     double StepSize = Width / Steps;
     double Guess = P_h_0; //Guess for P_h(0)
@@ -55,8 +110,8 @@ std::vector<std::vector<double>> avaProb(double Width, int Steps, double P_h_0, 
         
         for (int i = 0 ; i<Steps; i++){
             //Replace alpha/beta with alphaX(i*StepSize) later
-            P_e.at(i+1) = P_e.at(i) + (1- P_e.at(i)) * alphaX(i*StepSize) * (P_e.at(i)+P_h.at(i)-P_e.at(i)*P_h.at(i)) * StepSize;
-            P_h.at(i+1) = P_h.at(i) - (1- P_h.at(i)) * betaX(i*StepSize) * (i*StepSize) * (P_h.at(i)+P_e.at(i)-P_e.at(i)*P_h.at(i)) * StepSize;
+            P_e.at(i+1) = P_e.at(i) + (1- P_e.at(i)) * alphaX(DeltaV, StepSize*i, Width) * (P_e.at(i)+P_h.at(i)-P_e.at(i)*P_h.at(i)) * StepSize;
+            P_h.at(i+1) = P_h.at(i) - (1- P_h.at(i)) * betaX(DeltaV, StepSize*i, Width) * (P_h.at(i)+P_e.at(i)-P_e.at(i)*P_h.at(i)) * StepSize;
         }
 
         if (std::abs(P_h.at(Steps)) < Accuracy){ //If P_h(Width) is close enough to 0, return the lists
@@ -87,19 +142,24 @@ std::vector<std::vector<double>> avaProb(double Width, int Steps, double P_h_0, 
             previousF = F;
         }
     }
-    std::cout << "No solution found" << std::endl;
-    Pair.push_back(P_e);
-    Pair.push_back(P_h);
+    //std::cout << "No solution found" << std::endl;
+    //Pair.push_back(P_e);
+    //Pair.push_back(P_h);
     return Pair; //If the while loop ends, return the lists
 }
 
 int main(){
-    std::vector<std::vector<double>> Pair = avaProb(5, 1000, 0.5, 0.0001);
+    /*std::vector<std::vector<double>> Pair = avaProb(0.00005, 1000, 0.8, 0.0001);
     for (std::vector<double> P_x : Pair){
         std::cout << "List: " << std::endl;
         for (double Step : P_x){
             std::cout << Step << std::endl;
         }
     }
-    return 0;
+    return 0;*/
+
+    std::vector<double> F = guess100(0.00011, 10000, 2);
+    for (double Fs: F){
+        std::cout << Fs << std::endl;
+    }
 }
--- avaProb.h
+++ avaProb.h
@@ -4,8 +4,10 @@
 #include <cmath>
 #include <vector>
 
-double alphaX(double Pos);
+double alphaX(double DeltaV, double Pos, double Width);
 
-double betaX(double Pos);
+double betaX(double DeltaV, double Pos, double Width);
 
-std::vector<std::vector<double>> avaProb(double Width, int Steps, double P_h_0, double Accuracy);
+std::vector<double> guess100(double Width, int Steps, double DeltaV);
+
+std::vector<std::vector<double>> avaProb(double Width, int Steps, double P_h_0, double Accuracy, double DeltaV);
